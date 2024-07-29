fc089ca4d2e5f467493d2b5ffc3a072e625431e5
Nishit Chaudhary <90385616+nishit3@users.noreply.github.com>
2024-07-29 22:28:42 +0530

    Create EA-Satellite-Img-Provider.py

---
+++ AWS/Lambda/EA-Satellite-Img-Provider.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import http.client
+
+dynamoDB = boto3.resource('dynamodb')
+table = dynamoDB.Table("EA-Area-Wise-Things-Data")
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    
+    response = table.scan()["Items"]
+    bucket_name = 'ea-area-wise-satellite-imgs'
+    
+    for area in response:
+        areaDetails = area["details"]
+        object_key = area["areaUID"]+".png"
+        lat = areaDetails['lat']
+        lng = areaDetails['lng']
+        
+        params = {
+            "center": f"{lat},{lng}",
+            "zoom": "14",
+            "size": "400x400",
+            "maptype": "satellite",
+            "key": "AIzaSyCrXQtp1P8g2eeZNE3Gm18sfiBdDgCoEZs"
+        }
+        query_string = "&".join([f"{key}={value}" for key, value in params.items()])
+        url = f"/maps/api/staticmap?{query_string}"
+        
+        conn = http.client.HTTPSConnection("maps.googleapis.com")
+        conn.request("GET", url)
+        response = conn.getresponse()
+        
+        if response.status == 200:
+            image_data = response.read()
+            s3.put_object(Bucket=bucket_name, Key=object_key, Body=image_data, ContentType='image/png')
+        
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Satellite imgs updated')
+    }
